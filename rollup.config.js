a03f4b83dfffc23ada48f682205b2e80b728b91e
VarNotUsed <varnotused@protonmail.com>
2025-05-13 18:47:30 +0200

    formatting

--- rollup.config.js
+++ rollup.config.js
@@ -6,42 +6,42 @@ import dts from "rollup-plugin-dts";
 import { readFileSync } from "node:fs";
 
 const pkg = JSON.parse(
-  readFileSync(new URL("./package.json", import.meta.url), "utf8"),
+	readFileSync(new URL("./package.json", import.meta.url), "utf8"),
 );
 
 const config = [
-  {
-    input: "src/index.ts",
-    output: [
-      {
-        file: pkg.module,
-        format: "esm",
-        sourcemap: true,
-      },
-      {
-        file: pkg.main,
-        format: "cjs",
-        sourcemap: true,
-      }
-    ],
-    plugins: [
-      resolve(),
-      commonjs(),
-      typescript({
-        tsconfig: "./tsconfig.json"
-      }),
-      terser(),
-    ],
-    external: ["react", "react-dom"]
-  },
-  {
-    input: "src/index.ts",
-    output: {
-      file: "dist/index.d.ts",
-      format: "es"
-    },
-    plugins: [dts()]
-  }
+	{
+		input: "src/index.ts",
+		output: [
+			{
+				file: pkg.module,
+				format: "esm",
+				sourcemap: true,
+			},
+			{
+				file: pkg.main,
+				format: "cjs",
+				sourcemap: true,
+			},
+		],
+		plugins: [
+			resolve(),
+			commonjs(),
+			typescript({
+				tsconfig: "./tsconfig.json",
+			}),
+			terser(),
+		],
+		external: ["react", "react-dom"],
+	},
+	{
+		input: "src/index.ts",
+		output: {
+			file: "dist/index.d.ts",
+			format: "es",
+		},
+		plugins: [dts()],
+	},
 ];
 
 export default config;
